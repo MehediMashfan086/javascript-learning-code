28496aca47a50b0e10d874bcbdf64d1a358bc393
Mehedi Hasan <89673282+MehediMashfan086@users.noreply.github.com>
2023-10-11 06:51:06 +0000

    feat(variable): const, let, var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 11162086
+let accountEmail = "mehedihasan62086@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Dhaka"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "mh@gmail.com"
+accountPassword = "456"
+accountCity = "Rajshahi"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
